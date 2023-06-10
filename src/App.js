36756cee922a76cdffd8bb3a209e1e666b8a94e3
fakhrifebriansyah <fakhrifebriansya8@gmail.com>
2023-06-11 03:43:19 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { sendMessageToOpenAI } from './openai';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleSend = async () => {
+    const response = await sendMessageToOpenAI(input);
+    setMessages([
+      ...messages,
+      {text: input, isUser: true},
+      {text: response, isUser: false}
+    ]);
+    setInput("");
+    console.log(response);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="chat">
+        {messages.map((message, index) => (
+          <div key={index} className={message.isUser ? "user-message" : "bot-message"}>{message.text}</div>
+        ))}
+      </div>
+      
+
+      <div class="input-container">
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)}></input>
+        <button onClick={handleSend}>Send</button>
+      </div>
     </div>
   );
 }
---
+++ src/openai.js
@@ -0,0 +1,22 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const API_KEY = "YOUR API_KEY";
+
+const configuration = new Configuration({
+    apiKey: API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export async function sendMessageToOpenAI(message) {
+    const response = await openai.createCompletion({
+        model: 'text-davinci-003', 
+        prompt: message, 
+        temperature: 0.9, 
+        max_tokens: 256,
+        top_p: 1, 
+        frequency_penalty: 0,
+        presence_penalty: 0
+    })
+    return response.data.choices[0].text
+}
